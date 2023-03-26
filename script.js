33e3fc8746d32ac22836fb15363c8ed5efc3050c
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-26 16:46:14 +0500

    Add js

--- script.js
+++ script.js
@@ -100,20 +100,25 @@ function updateDOM() {
   })
 
   // Run getSavedColumns only once, Update Local Storage
-
+  updatedOnLoad = true;
+  updateSavedColumns();
 }
 
 // allow arrays
 function rebuildArrays() {
+  backlogListArray = [];
   for (let i = 0; i < backlogList.children.length; i++) {
     backlogListArray.push(backlogList.children[i].textContent);    
   }
+  progressListArray = [];
   for (let i = 0; i < progressList.children.length; i++) {
     progressListArray.push(progressList.children[i].textContent);    
   }
+  completeListArray = [];
   for (let i = 0; i < completeList.children.length; i++) {
     completeListArray.push(completeList.children[i].textContent);    
   }
+  onHoldListArray = [];
   for (let i = 0; i < onHoldList.children.length; i++) {
     onHoldListArray.push(onHoldList.children[i].textContent);    
   }
